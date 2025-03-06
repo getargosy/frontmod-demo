2628fc6ffaee115d5721b0e53efc49b750e6b61b
Emily Kauffman <kauffmanes@gmail.com>
2025-03-06 10:46:26 -0500

    more violations

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import TodoList from './components/TodoList';
+import Settings from './components/Settings';
 
 // Intentional ESLint violations:
 // 1. Missing return type annotation
@@ -17,11 +19,36 @@ const App = () => {
   };
 
   return (
-    <div className='app'>
-      <h1>Todo List</h1>
-      <TodoList todos={todos} setTodos={setTodos} />
-      <button onClick={() => addTodo('New Todo')}>Add Todo</button>
-    </div>
+    <Router>
+      <div className='app'>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Todo List</Link>
+            </li>
+            <li>
+              <Link to='/settings'>Settings</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path='/'>
+            <Route
+              index
+              element={
+                <>
+                  <h1>Todo List</h1>
+                  <TodoList todos={todos} setTodos={setTodos} />
+                  <button onClick={() => addTodo('New Todo')}>Add Todo</button>
+                </>
+              }
+            />
+            <Route path='settings' element={<Settings />} />
+          </Route>
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
